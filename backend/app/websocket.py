a8dd332b7df5aa67849285c6be9fe3f4429b80dc
bniu <56135819+bniu-amz@users.noreply.github.com>
2023-11-14 17:35:13 -0800

    Update websocket.py

--- backend/app/websocket.py
+++ backend/app/websocket.py
@@ -79,17 +79,12 @@ def handler(event, context):
 
     logger.debug(f"invoke bedrock payload: {payload}")
 
-    
-
     prompt_template = """
-
     Human: Use the following pieces of context to provide a concise answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
-
     {context}
-
     Question: {question}
-
     Assistant:"""
+  
     PROMPT = PromptTemplate(
         template=prompt_template, input_variables=["context", "question"]
     )
@@ -103,8 +98,10 @@ def handler(event, context):
         return_source_documents=True,
         chain_type_kwargs={"prompt": PROMPT}
     )
-    query = chat_input.message.content.body
-    concatenated = qa({"query": query})
+    
+    logger.debug(f"invoke bedrock query: {message}")
+    
+    concatenated = qa({"query": message})
     
 
     # Append entire completion as the last message
